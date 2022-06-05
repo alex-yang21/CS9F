6052a80ea1445fbc443c3754f36b639dc5a6df67
Alex Yang <alex.yang@berkeley.edu>
2022-06-05 02:25:50 -0700

    started proj1b

--- proj1b/cat+mouse.cpp
+++ proj1b/cat+mouse.cpp
@@ -8,9 +8,38 @@ using namespace std;
  * It should read legal cat and mouse positions from the user
  * and return the position values in its two arguments.
  */
-void GetPositions(/* TODO: Your code here */)
+
+/* Notes from specs
+  - each second is one time step, if more than 30 timesteps, then cat fails
+  - the cat moves first
+  - the cat's radius can never be less than 1 (it cannot move into the base)
+  - It should print the position of the cat and mouse after each move,
+    followed by the results of the chase (either the mouse becomes lunch, or the cat gives up after 30 minutes)
+  - The cat sees the mouse if (cat radius) * cos (cat angle - mouse angle) >= 1.0 (angles in radian)
+  - When the cat circles distance d around the statue, its radius does not change,
+    and the change in its angle can be calculated from the relationship d = angle * radius (angles in radian)
+  - The cat catches the mouse when it (the cat) moves past the mouse while at the base of the statue,
+    i.e. when the cat radius is 1.0 and the mouse angle lies between the old cat angle and the new cat angle.
+    An angle B is between angles A and C in the following circumstances (both):
+      - cos (B - A) > cos (C - A)
+      - cos (C - B) > cos (C - A) (angles in radian)
+    The difference C - A is assumed to be less than 90°, or π/2 radians.
+*/
+
+// M_PI is pi
+
+void GetPositions(Position cat_position, Position mouse_position)
 {
     // TODO: Your code here
+    // take in input from the user for the mouse (just angle since it has a 1m radius) and cat (angle and radius from statue)
+    float cat_angle;
+    float cat_radius;
+    float mouse_angle;
+
+
+
+    // set the private attributes of the Position objects
+
 }
 
 /**
@@ -24,6 +53,16 @@ void GetPositions(/* TODO: Your code here */)
 void RunChase(/* TODO: Your code here */)
 {
     // TODO: Your code here
+    int timer = 30;
+
+    while (timer > 0) {
+      // check if cat can eat mouse
+
+      // update cat and mouse positions
+      timer -= 1;
+    }
+
+    // cat gives up
 }
 
 int main()
--- proj1b/positions.cpp
+++ proj1b/positions.cpp
@@ -1,1 +1,66 @@
 // TODO: Your code here
+#include <iostream>
+#include <positions.h>
+
+// constructor 1
+Position::Position()
+: myRadius(1), myAngleInRadians(0))
+{
+  // default values if no parameters passed in
+}
+
+// constructor 2
+Position::Position(float r)
+: myRadius(r), myAngleInRadians(0)
+{
+  // assign radius, default angle is 0
+}
+
+// constructor 3
+Position::Position(float r, float thetaInRadians)
+: myRadius(r), myAngleInRadians(thetaInRadians)
+{
+  // assign both radius and angle
+}
+
+// beginning of member functions
+void Position::SetAbsolutePosition(float r, float thetaInRadians) {
+  myRadius = r;
+  myAngleInRadians = thetaInRadians;
+}
+
+void Position::IncrementPosition(float rChange, float angularDistChange) {
+  if (rChange != 0 && angularDistChange != 0) {
+    cout << "At least one argument must be 0.";
+    return;
+  }
+  myRadius += rChange;
+  myAngleInRadians += angularDistChange;
+}
+
+void Position::Print() const { // const means it can't modify anything
+  cout << "Radius: " << myRadius << "\n";
+  cout << "Angle: " << myAngleInRadians << "\n";
+}
+
+bool Position::Sees(Position pos) const {
+
+  if (!pos.IsAtStatue()) { // argument position must be at the statue
+    return false;
+  }
+
+
+
+}
+
+bool Position::IsAtStatue() const {
+  if (myRadius <= 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool Position::IsBetween(Position pos1, Position pos2) const {
+
+}
