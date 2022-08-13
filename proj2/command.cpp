11491295ce8b1243c892187589de4a85991d239e
Alex Yang <alex.yang@berkeley.edu>
2022-08-12 19:32:25 -0500

    finished task1

--- proj2/command.cpp
+++ proj2/command.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <cstdlib>
 using namespace std;
 
 // Function Prototypes
@@ -33,13 +34,13 @@ void InterpretCommands(istream& cmds)
     string line, lineInWord;
     // TODO: Declare other variables here.
 
-    while (________) // TODO: Get a line.
+    while (getline(cmds, line)) // TODO: Get a line.
     {
-        istringstream lineIn(line);
+        istringstream lineIn(line.c_str()); // this makes a copy of line as a string stream, requires C string
 
-        if (________) // TODO: Get the line's first word.
+        if (!(lineIn >> lineInWord)) // TODO: Get the line's first word.
         {
-            ________; // TODO: Do something if there weren't any words.
+            cerr << "No words given." << endl; // TODO: Do something if there weren't any words.
         }
         else if (lineInWord == "update")
         {
@@ -62,4 +63,61 @@ void InterpretCommands(istream& cmds)
             cerr << "Unrecognizable command word." << endl;
         }
     }
+}
+
+void InterpretUpdate(istream& cmd) {
+    string word;
+    int num;
+
+    if (!(cmd >> word >> num)) { // no other words given, error
+        cerr << "Update command requires a corresponding word and number." << endl;
+    }
+
+    cout << "Update " << word << " " << num << endl; // temp place holder
+
+    return;
+}
+
+void InterpretList(istream& cmd) {
+    string word;
+
+    if (!(cmd >> word)) {
+        cerr << "List requires another command 'names' or 'quantities'." << endl;
+    } else if (word == "names") {
+        ;
+    } else if (word == "quantities") {
+        ;
+    } else {
+        cerr << "Unrecognizable command word for 'list'." << endl;
+    }
+    return;
+}
+
+void InterpretBatch(istream& cmd) {
+    /* 
+    For the batch command, do the following: read, recognize, and process commands from the specified file, 
+    then (assuming that a quit command has not been read) resume reading commands from cin. 
+    Assume that the file does not contain any batch commands.
+    */ 
+    string fileName;
+
+    if (!(cmd >> fileName)) {
+        cerr << "Batch requires the name of a file." << endl;
+    }
+
+    ifstream file; 
+    file.open(fileName);
+    InterpretCommands(file);
+    file.close();
+
+    return;
+}
+
+void InterpretQuit(istream& cmd) {
+    string temp;
+    if (cmd >> temp) {
+        cerr << "Exit command should have no other arguments." << endl;
+    }
+    exit(0);
+    return;
 }
