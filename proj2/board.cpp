856ebe17ca1c8811e561dfc753e04c49beda9a1e
Alex Yang <alex.yang@berkeley.edu>
2022-08-14 02:07:52 -0500

    finished proj2

--- proj2/board.cpp
+++ proj2/board.cpp
@@ -1,56 +1,87 @@
-#include "board.h"
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-#define M 19
-#define N 19
+#define M 3
+#define N 3
 
-Board::Board() {
-    char initial = '.';
-    vector<vector<char>> board(M, vector<char>(N, initial));
-}
+class Board 
+{
+public:
+    Board() {
+        // vector<vector<char> > board(M, vector<char>(N, '.'));
 
-void Board::SetCell(int player, int row, int col, char c) {
-    if (player != orientation) { // rotate 180 degrees
-        rotate(board);
-        rotate(board);
-    } 
-    board[row][col] = c;
-    orientation = player; // we store the board's orientation based on the last call
-}
+		board.resize(M);
+		for (int i = 0; i < N; i++) {
+			board[i] = vector<char>(N);
+			for (int j = 0; j < N; j++) {
+				board[i][j] = '.';
+			}
+		}
+    }
 
-void Board::Print(int player) {
-    if (player != orientation) { // rotate 180 degrees
-        rotate(board);
-        rotate(board);
-    } 
-    // print the board
-    for (int i=0; i < M; i++) {
-        for (int j=0; j < N; j++) {
-            cout << board[i][j];
-            if (j != N-1) {
-                cout << " ";
+    void SetCell(int player, int row, int col, char c) {
+        if (player != 0) {
+            board[M-1-row][N-1-col] = c;
+        } else {
+            board[row][col] = c;
+        }
+    }
+
+    void Print(int player) {
+        if (player != 0) { 
+            // print the board backward
+            for (int i=M-1; i >= 0; i--) {
+                for (int j=N-1; j >= 0; j--) {
+                    cout << board[i][j];
+                    if (j != 0) {
+                        cout << " ";
+                    }
+                }
+                cout << endl;
+            }
+        } else {
+            // print the board forward
+            for (int i=0; i < M; i++) {
+                for (int j=0; j < N; j++) {
+                    cout << board[i][j];
+                    if (j != M-1) {
+                        cout << " ";
+                    }
+                }
+                cout << endl;
             }
         }
-        cout << endl;
-    }
-    orientation = player;
-}
-
-void rotate(vector<vector<char>>& board) {
-    // transpose matrix
-    for (int i=0; i < 19; i++) {
-        for (int j=0; j < 19; j++) {
-            swap(board[i][j], board[j][i]);
-        }
     }
 
-    // reverse columns
-    int i, j ,k;
-    for (i=0; i < 19 ;i++) {
-        k = 18;
-        for (j=0; j < 9; j++) {
-            swap(board[i][j],board[i][k--]);
-        }
-    }
+private:
+    vector<vector<char> > board;
+};
+
+int main() {
+    Board board;
+
+	board.Print(0);
+    cout << endl;
+	board.Print(1);
+    cout << endl;
+
+	board.SetCell(0, 0, 0, 'X');
+	board.SetCell(1, 0, 0, 'O');
+
+	board.Print(0);
+    cout << endl;
+	board.Print(1);
+    cout << endl;
+
+	board.SetCell(0, 1, 2, 'X');
+	board.SetCell(1, 0, 1, 'O');
+
+	board.Print(0);
+    cout << endl;
+	board.Print(1);
+    cout << endl;
+
+	return 0;
 }
